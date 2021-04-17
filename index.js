b367955d696d7f015adf93306dbe2069da911503
Rafał Maciejewski <rafaljestsuper@gmail.com>
2021-04-17 12:50:38 +0200

    fix: update stylelint rules
    BREAKING CHANGE: bumped required stylelint version from 8 to ^13.12.0

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 module.exports = {
   rules: {
-    'color-hex-case': 'upper',
+    'color-hex-case': 'lower',
     'color-hex-length': 'short',
     'color-named': 'never',
     'color-no-invalid-hex': true,
-
     'font-family-name-quotes': 'always-unless-keyword',
     'font-family-no-duplicate-names': true,
     'font-weight-notation': 'numeric',
-
     'function-calc-no-unspaced-operator': true,
     'function-comma-newline-after': 'never-multi-line',
     'function-comma-newline-before': 'never-multi-line',
@@ -26,34 +24,24 @@ module.exports = {
       },
     ],
     'function-whitespace-after': 'always',
-
     'number-max-precision': 4,
     'number-no-trailing-zeros': true,
-
     'string-no-newline': true,
     'string-quotes': 'single',
-
     'length-zero-no-unit': true,
-
     'unit-case': 'lower',
     'unit-no-unknown': true,
-
     'value-keyword-case': 'lower',
     'value-no-vendor-prefix': true,
     'value-list-comma-space-after': 'always',
     'value-list-comma-space-before': 'never',
     'value-list-max-empty-lines': 0,
-
     'shorthand-property-no-redundant-values': true,
-
     'property-case': 'lower',
     'property-no-unknown': true,
-    'property-no-vendor-prefix': true,
-
     'keyframe-declaration-no-important': true,
-
     'declaration-bang-space-after': 'never',
-    'declaration-bang-space-before': 'never',
+    'declaration-bang-space-before': 'always',
     'declaration-colon-space-after': 'always',
     'declaration-colon-space-before': 'never',
     'declaration-empty-line-before': [
@@ -63,7 +51,6 @@ module.exports = {
       },
     ],
     'declaration-no-important': true,
-
     'declaration-block-no-duplicate-properties': true,
     'declaration-block-no-redundant-longhand-properties': true,
     'declaration-block-no-shorthand-property-overrides': true,
@@ -78,7 +65,6 @@ module.exports = {
     'block-no-empty': true,
     'block-opening-brace-newline-after': 'always',
     'block-opening-brace-space-before': 'always',
-
     'selector-attribute-brackets-space-inside': 'never',
     'selector-attribute-operator-space-after': 'never',
     'selector-attribute-operator-space-before': 'never',
@@ -88,7 +74,6 @@ module.exports = {
     'selector-descendant-combinator-no-non-space': true,
     'selector-max-compound-selectors': 4,
     'selector-max-attribute': 0,
-    'selector-no-vendor-prefix': true,
     'selector-pseudo-class-case': 'lower',
     'selector-pseudo-class-no-unknown': true,
     'selector-pseudo-class-parentheses-space-inside': 'never',
@@ -98,22 +83,17 @@ module.exports = {
     'selector-type-case': 'lower',
     'selector-type-no-unknown': true,
     'selector-max-empty-lines': 0,
-
     'selector-list-comma-newline-after': 'always',
     'selector-list-comma-newline-before': 'never-multi-line',
-
     'media-feature-colon-space-after': 'always',
     'media-feature-colon-space-before': 'never',
     'media-feature-name-case': 'lower',
     'media-feature-name-no-unknown': true,
-    'media-feature-name-no-vendor-prefix': true,
     'media-feature-parentheses-space-inside': 'never',
     'media-feature-range-operator-space-after': 'always',
     'media-feature-range-operator-space-before': 'always',
-
     'media-query-list-comma-space-after': 'always',
     'media-query-list-comma-space-before': 'never',
-
     'at-rule-empty-line-before': [
       'always',
       {
@@ -132,16 +112,20 @@ module.exports = {
       {
         except: ['first-nested'],
         ignore: ['after-comment', 'stylelint-commands'],
+        ignoreComments: [
+          'critical-desktop:start',
+          'critical-desktop:end',
+          'critical-mobile:start',
+          'critical-mobile:end',
+        ],
       },
     ],
     'comment-no-empty': true,
-    'comment-whitespace-inside': 'always',
     'comment-word-blacklist': ['dupa'],
-
     indentation: 4,
     'max-empty-lines': 1,
     'max-line-length': 120,
-    'max-nesting-depth': 4,
+    'max-nesting-depth': 6,
     'no-duplicate-selectors': true,
     'no-empty-source': true,
     'no-eol-whitespace': [
@@ -152,6 +136,6 @@ module.exports = {
     ],
     'no-extra-semicolons': true,
     'no-missing-end-of-source-newline': true,
-    'no-unknown-animations': true,
+    'no-unknown-animations': [true, { severity: 'warning' }],
   },
 };
