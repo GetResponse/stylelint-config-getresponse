0ab4cdd43f70997d33475954f4cd210095f1317d
lsobolew <lsobolew@gmail.com>
2025-04-17 15:12:35 +0200

    feat: add support for sass and stylus
    Co-authored-by: Łukasz Sobolewski <lukasz.sobolewski@getresponse.com>

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 module.exports = {
-    extends: ['stylelint-config-standard', '@stylistic/stylelint-config'],
-    rules: {
-        'function-url-quotes': [
-            'always',
-            {
-                except: ['empty'],
-            },
-        ],
-        'selector-max-compound-selectors': 4,
-        'comment-word-disallowed-list': ['dupa'],
-        'max-nesting-depth': [3, { ignore: ['blockless-at-rules'] }],
-        'no-unknown-animations': [true, { severity: 'warning' }],
-        'color-named': 'never',
-
-        '@stylistic/max-line-length': 140,
-        '@stylistic/number-leading-zero': 'never',
-        '@stylistic/string-quotes': 'single',
-        '@stylistic/indentation': 4,
-        '@stylistic/named-grid-areas-alignment': true,
-        '@stylistic/at-rule-semicolon-space-before': 'never',
-    },
+    overrides: [
+        {
+            files: ['**/*.scss', '**/*.sass', '**/*.css'],
+            ...require('./css'),
+        },
+        {
+            files: ['**/*.scss', '**/*.sass'],
+            ...require('./sass'),
+        },
+        {
+            files: ['**/*.styl'],
+            ...require('./stylus'),
+        },
+    ],
 };
