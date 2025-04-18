ab864a43789ee46cadaa1ceaf9033aa1522b27df
Łukasz Sobolewski <lukasz.sobolewski@getresponse.com>
2025-04-18 07:44:42 +0200

    feat: overwrite problematic rules for stylus

--- stylus.js
+++ stylus.js
@@ -1,3 +1,7 @@
 module.exports = {
     extends: ['stylelint-config-standard', 'stylelint-stylus/standard'],
+    rules: {
+        'stylus/number-leading-zero': 'never',
+        'media-feature-range-notation': 'prefix',
+    },
 };
